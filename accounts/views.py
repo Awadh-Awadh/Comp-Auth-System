8a137304352835f17ea6a2128b9e1e222a18a0b5
Awadh-Awadh <awadhawadh84@yahoo.com>
2021-11-10 13:29:53 +0300

    add registration and sign in templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CustomCreationForm
 
 # Create your views here.
@@ -8,6 +8,9 @@ def log(request):
 def reg(request):
     if request.method == 'POST':
         form = CustomCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
     else:
         form = CustomCreationForm(request.POST)
     context = {
